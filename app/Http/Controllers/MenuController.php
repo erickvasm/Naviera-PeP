27733b82a17c76570d72d3050b418591df683627
StevRomo420 <esteban5420978@hotmail.com>
2021-11-09 21:48:46 -0800

    Se agrega la funcionalidad de iframe en la vista de menu

---
+++ app/Http/Controllers/MenuController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class MenuController extends Controller
+{
+    
+    public function mostrarMenu() {
+    	return View("menu.principal");
+    }
+
+}
