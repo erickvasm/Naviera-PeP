f40a69f1b7c340c7dc377c1c24c921c1102aa992
Cris <Cristeban76@gmail.com>
2021-12-01 12:45:55 -0600

    Modificaciones

--- app/Http/Controllers/NaveController.php
+++ app/Http/Controllers/NaveController.php
@@ -83,6 +83,9 @@ class NaveController extends Controller
                     $date_input =strtotime($request->fecha); 
                     $provided_date = date('Y/m/d H:i', strtotime('+0 year, +0 days', $date_input));
 
+
+                    error_log("Message:");
+
                     error_log($provided_date);
 
                     if($provided_date>=$zarpado){
